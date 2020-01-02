5b68378f983b252d792d401f1cf88de5a2c186f2
Dave Della Costa <dave@reifyhealth.com>
2020-01-02 13:48:36 -0500

    add set-ns key alias for cider-mode

--- dotemacs/clojure.el
+++ dotemacs/clojure.el
@@ -10,6 +10,12 @@
     :config
     (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
     (setq cider-repl-history-file "~/.emacs.d/cider-history.eld")
-    (setq cider-show-error-buffer nil))
+    (setq cider-show-error-buffer nil)
 
-  )
+    (add-hook 'cider-mode-hook
+              (lambda ()
+                (define-key cider-mode-map (kbd "C-c C-n") 'cider-repl-set-ns)))
+
+    ) ; cider-mode
+
+  ) ; clojure-mode
